4c84eb2990b5c54afebf0a50a53c4daa670537a9
Faizan Ali <faizanali3n@gmail.com>
2025-10-08 16:02:28 +0500

    Updated App.jsx

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div style={{ minHeight: '100vh', minWidth: '100vw', backgroundColor: color }} className="w-full h-screen duration-200">
       <div className="fixed flex flex-wrap justify-center bottom-12 insert-z-0 px-2 gap-3">
-        <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => setColor('red')}>Red</button>
+        <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={() => setColor('yellow')}>Yellow</button>
         <button className="bg-green-500 text-white px-4 py-2 rounded" onClick={() => setColor('green')}>Green</button>
         <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => setColor('blue')}>Blue</button>
       </div>
